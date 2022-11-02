665f809796c6b76ed52bb53cddd8aa4faeac83ec
Ginga Kennis <102468399+Ginga-Kennis@users.noreply.github.com>
2022-11-02 13:52:05 +0900

    Time: 74 ms (9.17%), Space: 13.8 MB (96.69%) - LeetHub

--- 0066-plus-one/0066-plus-one.py
+++ 0066-plus-one/0066-plus-one.py
@@ -5,6 +5,7 @@ class Solution:
         
         for i,elm in enumerate(digits):
             num += elm * (10**(l - i - 1))
+        
         return [int(i) for i in str(num+1) ]
         
         
